a07cd0b2eec1ae7add3fb158e3cd3822888187d4
ehsanziyaee <ehsan.ziyaee@gmail.com>
2024-06-27 14:16:08 +0330

    .

--- SpotifyInterface.c
+++ SpotifyInterface.c
@@ -345,7 +345,7 @@ bool Spotify_SendCommand(SpotifyInterfaceHandler_t SpotifyInterfaceHandler, int 
             retValue = true;
             break;
 
-        case GetUserInfo: 
+        case GetUserInfo:
             Spotify_GetInfo(Command, PrivateHandler.token.AccessToken);
             IsSuccessfull = PrivateHandler.SpotifyBuffer.status == 200;
             if (!IsSuccessfull)
@@ -354,8 +354,7 @@ bool Spotify_SendCommand(SpotifyInterfaceHandler_t SpotifyInterfaceHandler, int 
                 retValue = false;
                 break;
             }        
-            ExtractUserInfoParamsfromJson(PrivateHandler.SpotifyBuffer.MessageBuffer,
-            InterfaceHandler->UserInfo);         
+            ExtractUserInfoParamsfromJson(PrivateHandler.SpotifyBuffer.MessageBuffer,InterfaceHandler->UserInfo);         
             retValue = true;
             break;
 
